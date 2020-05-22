ac88c9875e700ad7df4e30ce4152582d151c3bfe
Kathryn Rowe <kjrowe06@gmail.com>
2020-05-22 12:45:41 -0700

    makes species specific directories and files

---
+++ README.md
@@ -0,0 +1,7 @@
+```
+python3 split.py -i new_test.txt --output-dir test_texts/
+```
+
+```
+pdf2txt.py -o new_test2.txt -p 20 East_Bay_Native_Plants_2014_15.pdf
+```
--- split.py
+++ split.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+'''
+Iterate through text file that was translated from PDF.
+
+python3 split.py -i new_test.txt --output-dir test_texts/
+'''
+
 import os
 import argparse
 
@@ -15,21 +21,61 @@ parser.add_argument("-o", "--output-dir", required=True, help="output directory"
 
 args = parser.parse_args()
 
+# if the given output directory does not exist, make it
+if not os.path.isdir(args.output_dir):
+    os.mkdir(args.output_dir)
+current_directory = os.getcwd()
+working_dir = os.path.join(current_directory, args.output_dir)
+
 # read the input file
 with open(args.input_file, 'r') as input_file:
     input_data = input_file.read()
-    # print(input_data)
-    # input_lines = input_data.splitlines()
+
+    # split pages by special character
+    single_page = input_data.split(page_delimiter)
+
     page_number = 0
-    for item in input_data:
-        if input_data == page_delimiter:
-            out_file = open(f'break_pg_{page_number}', 'w')
-        elif input_data != page_delimiter:
-            print(page_number)
-            out_file.write(input_data)
-        page_number += 1
-        if page_number == 5:
-            quit()
+    for item in single_page:
+        
+        # base directory where every species is stored
+        os.chdir(working_dir)
+
+        single_page_list = item.splitlines(0)
+        
+        # ignore empty pages
+        if len(single_page_list) > 0:
+            # remove leading and trailing whitespace
+            new = [ i.strip() for i in single_page_list ]
+
+            # remove empty spaces
+            new = list(filter(None, new))
+
+            # remove first item in list if it's obviously not a plant name
+            if len(new[0]) >= 100 or new[0][0:5] in ['Photo', 'Main ', 'Flowe', ',    ', 
+                                                     'Right', 'Seed ', 'Weber', 'Wild ']:
+                new = new[1:]
+
+            # some plant names contain brackets. remove them for easier directory access
+            # replace spaces with underscores for file and directory snake case
+            plant_name = ''
+            for char in new[0]:
+                if char != '[' and char != ']': 
+                    if char == ' ':
+                        char = '_'
+                    plant_name = plant_name + char
+
+            # change to directory
+            species_dir = plant_name + f"_pg{page_number}" 
+            os.mkdir(species_dir)
+            os.chdir(species_dir)
+
+            # save file with species name. first item in page list
+            species_file = f'{plant_name}_pg_{page_number}.txt'
+            out_file = open(species_file, 'w')
+            out_file.write(str(new))
+            out_file.close()
+            page_number += 1
+
             
 # # iterate through the input data by line
 # input_lines = input_data.splitlines()
